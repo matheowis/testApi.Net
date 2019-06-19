ab6bff861b172b54cc440639ebb94c9b648cc068
Wiśniowski Mateusz <mateuszwis93@gmail.com>
2019-06-19 08:42:21 +0200

    Initial commit

---
+++ testAPI/Controllers/MateuszController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testAPI.Repository;
+
+namespace testAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MateuszController : ControllerBase
+    {
+        [HttpGet]
+        [Route("act")]
+        public ActionResult Action()
+        {
+            var repo = new PersonRepository();
+            var people = repo.GetPeople();
+            return Ok(people);
+        }
+    }
+}
---
+++ testAPI/Repository/PersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testAPI.Models;
+
+namespace testAPI.Repository
+{
+    public class PersonRepository:Service
+    {
+        public List<Person> GetPeople()
+        {
+            return Db.Select<Person>().ToList();
+        }
+    }
+}
